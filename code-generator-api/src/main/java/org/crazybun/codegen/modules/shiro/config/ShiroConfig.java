f31020fec23819b6d0b19ac86d2e54f1b1a693be
baobao222222@qq.com <baobao222222@qq.com>
2020-03-05 11:20:11 +0800

    Spring boot 更新 2.2 版本后需要同步同步更新 Shiro-Redis 版本, 设置 IP 与设置端口合并了

--- code-generator-api/src/main/java/org/crazybun/codegen/modules/shiro/config/ShiroConfig.java
+++ code-generator-api/src/main/java/org/crazybun/codegen/modules/shiro/config/ShiroConfig.java
@@ -1,5 +1,11 @@
 package org.crazybun.codegen.modules.shiro.config;
 
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.crazybun.codegen.modules.shiro.ShiroRealm;
 import org.crazybun.codegen.modules.shiro.ShiroSessionIdGenerator;
 import org.crazybun.codegen.modules.shiro.ShiroSessionManager;
@@ -8,12 +14,6 @@ import org.crazybun.codegen.modules.shiro.filter.MyRolesAuthorizationFilter;
 import org.crazybun.codegen.modules.shiro.filter.TokenCheckFilter;
 import org.crazybun.codegen.modules.shiro.service.impl.ShiroServiceImpl;
 import org.crazybun.codegen.modules.shiro.utils.SHA256Util;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.session.mgt.SessionManager;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.crazycake.shiro.RedisCacheManager;
 import org.crazycake.shiro.RedisManager;
 import org.crazycake.shiro.RedisSessionDAO;
@@ -26,10 +26,10 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- *  <p> Shiro配置类 </p>
+ * <p> Shiro配置类 </p>
  *
- * @description :
  * @author : CrazyBunQnQ
+ * @description :
  * @date : 2019/8/23 15:52
  */
 @Configuration
@@ -40,7 +40,7 @@ public class ShiroConfig {
     private final int EXPIRE = 1800;
 
     /**
-     *  Redis配置
+     * Redis配置
      */
     @Value("${spring.redis.host}")
     private String host;
@@ -48,8 +48,8 @@ public class ShiroConfig {
     private int port;
     @Value("${spring.redis.timeout}")
     private int timeout;
-//    @Value("${spring.redis.password}")
-//    private String password;
+    //    @Value("${spring.redis.password}")
+    //    private String password;
 
     /**
      * 开启Shiro-aop注解支持：使用代理方式所以需要开启代码支持
@@ -65,26 +65,26 @@ public class ShiroConfig {
      * Shiro基础配置
      */
     @Bean
-    public ShiroFilterFactoryBean shiroFilterFactory(SecurityManager securityManager, ShiroServiceImpl shiroConfig){
+    public ShiroFilterFactoryBean shiroFilterFactory(SecurityManager securityManager, ShiroServiceImpl shiroConfig) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean.setSecurityManager(securityManager);
 
         // 自定义过滤器
         Map<String, Filter> filtersMap = new LinkedHashMap<>();
         // 定义过滤器名称 【注：map里面key值对于的value要为authc才能使用自定义的过滤器】
-        filtersMap.put( "zqPerms", new MyPermissionsAuthorizationFilter() );
-        filtersMap.put( "zqRoles", new MyRolesAuthorizationFilter() );
-        filtersMap.put( "token", new TokenCheckFilter() );
+        filtersMap.put("zqPerms", new MyPermissionsAuthorizationFilter());
+        filtersMap.put("zqRoles", new MyRolesAuthorizationFilter());
+        filtersMap.put("token", new TokenCheckFilter());
         shiroFilterFactoryBean.setFilters(filtersMap);
 
         // 登录的路径: 如果你没有登录则会跳到这个页面中 - 如果没有设置值则会默认跳转到工程根目录下的"/login.jsp"页面 或 "/login" 映射
         shiroFilterFactoryBean.setLoginUrl("/api/auth/unLogin");
         // 登录成功后跳转的主页面 （这里没用，前端vue控制了跳转）
-//        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //        shiroFilterFactoryBean.setSuccessUrl("/index");
         // 设置没有权限时跳转的url
         shiroFilterFactoryBean.setUnauthorizedUrl("/api/auth/unauth");
 
-        shiroFilterFactoryBean.setFilterChainDefinitionMap( shiroConfig.loadFilterChainDefinitionMap() );
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(shiroConfig.loadFilterChainDefinitionMap());
         return shiroFilterFactoryBean;
     }
 
@@ -114,7 +114,7 @@ public class ShiroConfig {
     }
 
     /**
-     *  自定义Realm的加密规则 -> 凭证匹配器：将密码校验交给Shiro的SimpleAuthenticationInfo进行处理,在这里做匹配配置
+     * 自定义Realm的加密规则 -> 凭证匹配器：将密码校验交给Shiro的SimpleAuthenticationInfo进行处理,在这里做匹配配置
      */
     @Bean
     public HashedCredentialsMatcher hashedCredentialsMatcher() {
@@ -130,13 +130,13 @@ public class ShiroConfig {
      * Redis集群使用RedisClusterManager，单个Redis使用RedisManager
      * @param redisProperties
      */
-//    @Bean
-//    public RedisClusterManager redisManager(RedisProperties redisProperties) {
-//        RedisClusterManager redisManager = new RedisClusterManager();
-//        redisManager.setHost(redisProperties.getHost());
-//        redisManager.setPassword(redisProperties.getPassword());
-//        return redisManager;
-//    }
+    //    @Bean
+    //    public RedisClusterManager redisManager(RedisProperties redisProperties) {
+    //        RedisClusterManager redisManager = new RedisClusterManager();
+    //        redisManager.setHost(redisProperties.getHost());
+    //        redisManager.setPassword(redisProperties.getPassword());
+    //        return redisManager;
+    //    }
 
     /**
      * 配置Redis管理器：使用的是shiro-redis开源插件
@@ -144,10 +144,9 @@ public class ShiroConfig {
     @Bean
     public RedisManager redisManager() {
         RedisManager redisManager = new RedisManager();
-        redisManager.setHost(host);
-        redisManager.setPort(port);
+        redisManager.setHost(host + ":" + port);
         redisManager.setTimeout(timeout);
-//        redisManager.setPassword(password);
+        //        redisManager.setPassword(password);
         return redisManager;
     }
 
@@ -168,7 +167,7 @@ public class ShiroConfig {
      * SessionID生成器
      */
     @Bean
-    public ShiroSessionIdGenerator sessionIdGenerator(){
+    public ShiroSessionIdGenerator sessionIdGenerator() {
         return new ShiroSessionIdGenerator();
     }
 
